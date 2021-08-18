bb93d6b5b992785c25f9cd1810f9028681166706
Waseem Rakab <waseem@my-city.net>
2021-08-18 11:23:27 +0300

    ios updates

--- ios/Plugin/CapFirebaseDynamicLinks.swift
+++ ios/Plugin/CapFirebaseDynamicLinks.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 @objc public class CapFirebaseDynamicLinks: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        return value
-    }
+    
 }
--- ios/Plugin/CapFirebaseDynamicLinksPlugin.swift
+++ ios/Plugin/CapFirebaseDynamicLinksPlugin.swift
@@ -1,5 +1,7 @@
 import Foundation
 import Capacitor
+import FirebaseCore
+import FirebaseDynamicLinks
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -9,10 +11,56 @@ import Capacitor
 public class CapFirebaseDynamicLinksPlugin: CAPPlugin {
     private let implementation = CapFirebaseDynamicLinks()
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+    
+    public override func load() {
+        if(FirebaseApp.app() == nil) {
+            FirebaseApp.configure()
+        }
+        
+        NotificationCenter.default
+            .addObserver(self, selector: #selector(self.handleUrlOpened(notification:)),
+                         name: Notification.Name.capacitorOpenURL, object: nil)
+        NotificationCenter.default
+            .addObserver(self, selector: #selector(self.handleUniversalLink(notification:)),
+                         name: Notification.Name.capacitorOpenUniversalLink, object: nil)
     }
+    
+    @objc public func handleUrlOpened(notification: NSNotification) {
+        guard let object = notification.object as? [String:Any?] else {
+            return
+        }
+        let url = object["url"] as! URL
+        DynamicLinks.dynamicLinks()
+            .dynamicLink(fromUniversalLink: url,
+                         completion: {(result, error) in
+                            if(result == nil) {
+                                return
+                            }
+                            let dynamicLink = result!
+                            
+                            let url = dynamicLink.url?.absoluteString ?? ""
+                            let timeStamp = NSDate().timeIntervalSince1970
+                            self.notifyListeners("onDynamicLink", data: ["url": url,"timestampClicked": timeStamp],
+                                                 retainUntilConsumed: true)
+        })
+    }
+    
+    @objc public func handleUniversalLink(notification: NSNotification) {
+        guard let object = notification.object as? [String:Any?] else {
+                    return
+        }
+        DynamicLinks.dynamicLinks()
+            .handleUniversalLink(object["url"] as! URL, completion: {(result,error) in
+                if(result == nil) {
+                    return
+                }
+                let dynamicLink = result!
+                
+                let url = dynamicLink.url?.absoluteString ?? ""
+                let timeStamp = NSDate().timeIntervalSince1970
+                self.notifyListeners("onDynamicLink", data: ["url": url,"timestampClicked": timeStamp],
+                                     retainUntilConsumed: true)
+            })
+    }
+    
 }
